91198751462fc665e2620b544fd8e4f06fa5cd9b
junho <kimjjun1007@gmail.com>
2021-11-16 08:39:53 +0900

    :wrench:1Act1Frag

---
+++ app/src/main/java/com/example/sns_super_android/chat/MakeChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sns_super_android.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sns_super_android.R
+
+class MakeChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_chat)
+    }
+}
