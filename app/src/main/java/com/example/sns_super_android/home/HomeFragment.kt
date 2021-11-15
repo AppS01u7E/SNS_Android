b8d54a44c4352b2fa7c050b6688b877db744e6e4
junho <kimjjun1007@gmail.com>
2021-11-16 07:44:23 +0900

    :wrench:Add3ActAnd4Frag

---
+++ app/src/main/java/com/example/sns_super_android/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.sns_super_android.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sns_super_android.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+
+    }
+}
---
+++ app/src/main/java/com/example/sns_super_android/soom/SoomFragment.kt
@@ -0,0 +1,24 @@
+package com.example.sns_super_android.soom
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sns_super_android.R
+
+class SoomFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_soom, container, false)
+    }
+
+    companion object {
+        
+    }
+}
