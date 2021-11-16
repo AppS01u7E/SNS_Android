1b9bbc7246b1775895feac8fb7fd336227f97734
junho <kimjjun1007@gmail.com>
2021-11-16 11:23:19 +0900

    :sparkles:MainAct

--- app/src/main/java/com/example/sns_super_android/MainActivity.kt
+++ app/src/main/java/com/example/sns_super_android/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.sns_super_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sns_super_android.chat.findchat.FindChatFragment
+import com.example.sns_super_android.chat.mainchat.ChatFragment
+import com.example.sns_super_android.chat.makechat.MakeChatActivity
+import com.example.sns_super_android.mypage.MyPageFragment
+import com.example.sns_super_android.soom.SoomFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        chatBtn.setOnClickListener {
+            tran(1)
+        }
+        soomBtn.setOnClickListener {
+            tran(2)
+        }
+        mypageBtn.setOnClickListener {
+            tran(3)
+        }
     }
+
+    private fun tran(num:Int){
+        val transaction = supportFragmentManager.beginTransaction()
+        when(num){
+            1->{
+                transaction.replace(R.id.main,FindChatFragment()).commit()
+            }
+            2->{
+                transaction.replace(R.id.main,SoomFragment()).commit()
+            }
+            3->{
+                transaction.replace(R.id.main,MyPageFragment()).commit()
+            }
+        }
+    }
+
 }
